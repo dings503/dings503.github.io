5035cc2980b28dea7816e7a6ec9d1ded681152a5
dings503 <2855176771@qq.com>
2016-05-14 21:51:44 +0800

    Site updated: 2016-05-14 21:51:44

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -2,13 +2,4 @@ $(document).ready(function () {
   var sidebarTop = $('.header-inner').height() + 10;
 
   $('#sidebar').css({ 'margin-top': sidebarTop }).show();
-
-  require([], function (){
-  	if($(".instagram").length) {
-    	require(['/js/photo.js', '/fancybox/jquery.fancybox.js', '/js/jquery.lazyload.js'], function(obj) {
-        obj.init();
-    	});
-	}
-  });
-
 });
